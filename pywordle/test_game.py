7b749d72c0b896e43644d9a2666b3e68e8a97d42
Alison Gale <agale753@gmail.com>
2022-02-26 06:34:54 -0800

    Add outlines for test cases

--- pywordle/test_game.py
+++ pywordle/test_game.py
@@ -5,9 +5,32 @@ from game import Game
 
 class TestGame(unittest.TestCase):
 
-    def test_solution(self):
-        game = Game("foo", False)
-        self.assertEqual(game.solution, "foo")
+    def test_guess(self):
+        self.assertTrue(False)
+
+    def test_guess_hard_mode_exception(self):
+        self.assertTrue(False)
+
+    def test_guess_invalid_word(self):
+        self.assertTrue(False)
+
+    def test_is_valid(self):
+        self.assertTrue(False)
+
+    def test_is_valid_hard_mode(self):
+        self.assertTrue(False)
+
+    def test_get_game_status_won(self):
+        self.assertTrue(False)
+    
+    def test_get_game_status_lost(self):
+        self.assertTrue(False)
+
+    def test_get_game_status_in_progress(self):
+        self.assertTrue(False)
+
+    def test_str(self):
+        self.assertTrue(False)
 
 
 if __name__ == '__main__':
--- pywordle/test_wordle.py
+++ pywordle/test_wordle.py
@@ -5,9 +5,23 @@ from wordle import Wordle
 
 class TestWordle(unittest.TestCase):
 
-    def test_value(self):
-        wordle = Wordle(["foo", "bar", "baz"])
-        self.assertListEqual(wordle.solutions, ["foo", "bar", "baz"])
+    def test_start_game(self):
+        self.assertTrue(False)
+
+    def test_get_valid_guesses(self):
+        self.assertTrue(False)
+
+    def test_get_valid_guesses_hard_mode(self):
+        self.assertTrue(False)
+
+    def test_get_valid_guesses_empty_result(self):
+        self.assertTrue(False)
+
+    def test_get_possible_solutions(self):
+        self.assertTrue(False)
+
+    def test_get_possible_solutions_hard_mode(self):
+        self.assertTrue(False)
 
 
 if __name__ == '__main__':
