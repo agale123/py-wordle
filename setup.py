1643c1370db703396d5f4dee4f8fa8beaf718a8c
Alison Gale <agale753@gmail.com>
2022-03-09 17:14:37 -0800

    Fix setup.py to properly install dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='pywordle',
       version='1.0',
       description='Wordle Game Engine',
       packages=['pywordle'],
-      install_requires=[
+      requires=[
           "termcolor",
           "enum",
           "matplotlib",
